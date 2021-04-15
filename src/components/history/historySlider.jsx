204fa6df7a065369e12859dd98911b208bc37b88
jork41989 <jork41989@gmail.com>
2021-04-15 14:01:55 -0400

    fixed responsiveness of the carosel

--- src/components/history/historySlider.jsx
+++ src/components/history/historySlider.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Carousel from "react-material-ui-carousel";
 import { CardMedia } from "@material-ui/core";
+import { useMediaQuery } from "react-responsive";
+
 
 export default function HistorySlider() {
-
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1050px)" });
   const cards = [
     <div
       style={{
@@ -115,9 +117,150 @@ export default function HistorySlider() {
     </div>,
   ];
 
+  const tinyCards = [
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        gridColumn: "2/span 2",
+      }}
+    >
+      <CardMedia
+        image={"/images/item1.png"}
+        style={{ width: "220px", height: "135px" }}
+      />
+    </div>,
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        gridColumn: "2/span 2",
+      }}
+    >
+      <CardMedia
+        image={"/images/item2.png"}
+        style={{ width: "220px", height: "135px" }}
+      />
+    </div>,
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        gridColumn: "2/span 2",
+      }}
+    >
+      <CardMedia
+        image={"/images/item1.png"}
+        style={{ width: "220px", height: "135px" }}
+      />
+    </div>,
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        gridColumn: "2/span 2",
+      }}
+    >
+      <CardMedia
+        image={"/images/item2.png"}
+        style={{ width: "220px", height: "135px" }}
+      />
+    </div>,
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        gridColumn: "2/span 2",
+      }}
+    >
+      <CardMedia
+        image={"/images/item1.png"}
+        style={{ width: "220px", height: "135px" }}
+      />
+    </div>,
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        gridColumn: "2/span 2",
+      }}
+    >
+      <CardMedia
+        image={"/images/item2.png"}
+        style={{ width: "220px", height: "135px" }}
+      />
+    </div>,
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        gridColumn: "2/span 2",
+      }}
+    >
+      <CardMedia
+        image={"/images/item1.png"}
+        style={{ width: "220px", height: "135px" }}
+      />
+    </div>,
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        gridColumn: "2/span 2",
+      }}
+    >
+      <CardMedia
+        image={"/images/item2.png"}
+        style={{ width: "220px", height: "135px" }}
+      />
+    </div>,
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        gridColumn: "2/span 2",
+      }}
+    >
+      <CardMedia
+        image={"/images/item1.png"}
+        style={{ width: "220px", height: "135px" }}
+      />
+    </div>,
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-evenly",
+        gridColumn: "2/span 2",
+      }}
+    >
+      <CardMedia
+        image={"/images/item2.png"}
+        style={{ width: "220px", height: "135px" }}
+      />
+    </div>,
+  ];
+
+  let sizeCheck = () => {
+    if(isTabletOrMobile){
+      return tinyCards
+    } else {
+      return cards
+    }
+  }
+
   return (
     <Carousel autoPlay={false} style={{gridColumn: "2/span 2"}}>
-      {cards}
+      {sizeCheck()}
     </Carousel>
   );
 }

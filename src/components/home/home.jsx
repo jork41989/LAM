1fcfe87086019cd70de8251d9d7e3803350b5908
jork41989 <jork41989@gmail.com>
2021-04-15 21:22:36 -0400

    media q for the nav

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -4,7 +4,7 @@ import './home.css'
 export default function Home() {
 
   return (
-    <div id='home' style={{background: 'url("/images/heroHome.png")', backgroundSize: "cover"}}>
+    <div id='home' style={{background: 'url("/images/heroHome.png")', backgroundSize: "cover", backgroundPosition: "center", backgroundRepeat:"no-repeat", backgroundPositionY: "center", backgroundPositionx: "center"}}>
 
     </div>
   )
